c595341d862cb9bd6557ea40bf7be581d550c6a5
qvstork <92053560+qvstork@users.noreply.github.com>
2022-01-17 21:49:36 +0200

    Add files via upload
    quite raw design yet

---
+++ Master/script.js
@@ -0,0 +1,62 @@
+const timer = document.getElementById('time');
+
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function timeStart() {
+	if (stoptime == true) {
+		stoptime = false;
+		timeCycle();
+	}
+}
+
+function timeStop() {
+	if (stoptime == false) {
+		stoptime = true;
+	}
+}
+
+function timeCycle() {
+	
+	if (stoptime == false) {
+	sec = parseInt(sec);
+	min = parseInt(min);
+	hr = parseInt(hr);	
+
+	sec = sec + 1;
+
+	if (sec==60) {
+		min = min + 1;
+		sec = 0;
+		}
+	if (min == 60) {
+		hr = hr + 1;
+		min = 0;
+		sec = 0;
+		}
+	if (sec<10) {
+		sec = "0" + sec;
+	}
+	if (min<10) {
+		min = "0" + min;
+	}
+	if (hr<10) {
+		hr = "0" + hr;
+	}
+	const timer = document.getElementById('time');
+ 	timer.innerHTML = hr + ':' + min + ':' + sec;
+ 	console.log(timer);
+    setTimeout("timeCycle()", 1000);
+
+	}
+}
+
+function timeReset() {
+    timer.innerHTML = "00:00:00";
+    stoptime = true;
+    hr = 0;
+    sec = 0;
+    min = 0;
+}
